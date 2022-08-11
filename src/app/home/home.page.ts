2116ec8f9bb8571ebc71a8aaeed01999b7e06757
Jute-tojo <tojoramaroson5@gmail.com>
2022-08-11 08:52:08 +0300

    ajout du projet

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Facebook ,FacebookLoginResponse } from '@awesome-cordova-plugins/facebook/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private fb:Facebook) {}
+
+facebook(){
+  this.fb.login(['public_profile', 'user_friends', 'email'])
+  .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
+  .catch(e => console.log('Error logging into Facebook', e));
+
+
+   this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
+}
+
+}
